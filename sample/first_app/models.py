dd15ef07ad694849455966d9329023e840bfab55
mfay <mfarahani1379@gmail.com>
2023-07-25 18:41:32 +0330

    start project

--- sample/first_app/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class Person(models.Model):
-    name = models.CharField(max_length=70)
-    age = models.IntegerField()
-    country = models.CharField(max_length=50)
